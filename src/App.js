fa7ecf13bca67d78a142e5a721abe58f9a2cb25e
PromitaDutta <promitadutta26@gmail.com>
2023-01-29 14:19:19 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/*import Welcome from './components/Welcome'
+
+const App = () => {
+  return <Welcome name="Promita" />
+}
+export default App*/
+
+import Counter from './components/Counter'
+
+const App = () => {
+  return <Counter />
+}
+export default App
